4bd14d308d98dbdb99147f13794db79063d44f4d
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2018-10-08 09:44:34 -0600

    Another commit for some reason

--- GroupLab.playground/Pages/Gabe's Page.xcplaygroundpage/Contents.swift
+++ GroupLab.playground/Pages/Gabe's Page.xcplaygroundpage/Contents.swift
@@ -1,6 +1,32 @@
 
 import Foundation
 
-var str = "Hello, playground"
+/*
+ In the countTo function, why doesn't i in the final print statement work? Comment your answer beneath the line of code. What could replace i so that it would always have the desired value? Rewrite the print statement in a way that works every time.
+ */
 
-//This is my page so go away.
+func countTo(num: Int) {
+    
+    for i in 1...num {
+        print(i)
+    }
+    
+    print("The final number was \(i)")
+}
+
+
+
+/*
+ 
+ */
+class Cake {
+    var flavor: String
+    var frosting: String
+    var tiers: Int
+    
+    init(flavor: String, frosting: String, tiers: Int) {
+        flavor = flavor
+        frosting = frosting
+        tiers = tiers
+    }
+}
